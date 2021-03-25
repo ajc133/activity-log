9219d8f1f7be37a0dd4b0dbbfc1af782aa7f3ed1
ajc133 <16407457+ajc133@users.noreply.github.com>
2021-03-25 12:57:44 -0700

    Write to csv
    This is 199% the wrong way to do this

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 
-function notifyButtonPress(buttonId, currState) {
+function submitButtons() {
   const xhr = new XMLHttpRequest();
   xhr.open('POST', '/activity', true);
   xhr.onload = () => {
@@ -9,30 +9,36 @@ function notifyButtonPress(buttonId, currState) {
       // console.log(res);
     }
   };
-  let pressed;
-  if (currState === 'selected') {
-    pressed = true;
-  } else {
-    pressed = false;
-  }
-  const json = {
-    button: buttonId,
-    pressed,
-  };
+  const buttons = document.querySelectorAll('.activity');
+  const buttonData = [];
+  [].forEach.call(buttons, (btn) => {
+    const today = new Date().toISOString();
+    const pressed = btn.classList.contains('selected');
+    const json = {
+      timestamp: today,
+      activity: btn.id,
+      pressed,
+    };
+    buttonData.push(json);
+  });
   xhr.onerror = () => {
     console.log('Network error');
   };
   xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify(json));
+  xhr.send(JSON.stringify({ activities: buttonData }));
+  console.log(JSON.stringify(buttonData));
 }
 
 function toggleActiveState() {
   this.classList.toggle('selected');
-  const currentState = this.className;
-  notifyButtonPress(this.id, currentState);
+  // const currentState = this.className;
+  // notifyButtonPress(this.id, currentState);
 }
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.activity');
 [].forEach.call(buttons, (btn) => {
-  btn.addEventListener('click', toggleActiveState, false);
+  btn.addEventListener('click', toggleActiveState);
 });
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', submitButtons);
--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 /* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
+const csv = require('csv-parser');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const app = express();
+
+const PORT = 9000;
+
+const csvWriter = createCsvWriter({
+  path: 'data.csv',
+  header: [
+    { id: 'timestamp', title: 'datetime' },
+    { id: 'activity', title: 'activity' },
+    { id: 'pressed', title: 'pressed' },
+  ],
+});
+
+function writeLines(activities) {
+  csvWriter
+    .writeRecords(activities)
+    .then(() => console.log('The CSV file was written successfully'));
+}
+
+function storeActivities(activities) {
+  writeLines(activities);
+}
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -11,10 +34,9 @@ app.get('/activities', (req, res) => {
 });
 
 app.post('/activity', (req, res) => {
-  const today = new Date().toISOString();
-  console.log(`${today},${req.body.button},${req.body.pressed}`);
+  storeActivities(req.body.activities);
   res.sendStatus(200);
   res.end();
 });
 
-app.listen(9000, () => { console.log('Started server'); });
+app.listen(9000, () => { console.log(`Started server on port ${PORT}`); });
