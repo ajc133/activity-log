fd8f9ea22eabe1764b49b80237190dbe3ffd0964
ajc133 <16407457+ajc133@users.noreply.github.com>
2021-04-13 17:09:15 -0700

    Ditch csv idea

--- server.js
+++ server.js
@@ -1,31 +1,9 @@
 /* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
-const csv = require('csv-parser');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const app = express();
-
 const PORT = 9000;
-
-const csvWriter = createCsvWriter({
-  path: 'data.csv',
-  header: [
-    { id: 'timestamp', title: 'datetime' },
-    { id: 'activity', title: 'activity' },
-    { id: 'pressed', title: 'pressed' },
-  ],
-});
-
-function writeLines(activities) {
-  csvWriter
-    .writeRecords(activities)
-    .then(() => console.log('The CSV file was written successfully'));
-}
-
-function storeActivities(activities) {
-  writeLines(activities);
-}
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -34,7 +12,6 @@ app.get('/activities', (req, res) => {
 });
 
 app.post('/activity', (req, res) => {
-  storeActivities(req.body.activities);
   res.sendStatus(200);
   res.end();
 });
